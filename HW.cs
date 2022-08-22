597d7a26bb5ec3a35be87de8f6eabc2934665321
ABiss <solyaris-2000@mail.ru>
2022-08-22 16:56:01 +0300

    Second task

--- HW.cs
+++ HW.cs
@@ -2,7 +2,7 @@
 int number = Convert.ToInt32(Console.ReadLine());
 double testResult = Math.Pow (number,2);
 Console.WriteLine("result " + testResult);
-*/
+
 
 //на вход 2 числа, вывод - max и min 
 int num1 = Convert.ToInt32(Console.ReadLine());
@@ -15,3 +15,24 @@ else
 {
     Console.WriteLine("max = " + num2 + " min = " + num1);
 }
+*/
+
+//Max из трех чисел
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if (num1>num2)
+{
+    max = num1;
+}
+else
+{
+    max = num2;
+}
+
+if (num3 > max)
+{
+    max = num3;
+}
+System.Console.WriteLine("Max value = " + max);
