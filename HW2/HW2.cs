a42fcb3a98c234f805d877c66241a0552d1801c0
ABiss <solyaris-2000@mail.ru>
2022-08-23 20:18:15 +0300

    1&2 tasks

---
+++ HW2/HW2.cs
@@ -0,0 +1,22 @@
+// на вход - трехзначное; вывод второй цифры
+/*
+int num = Convert.ToInt32(Console.ReadLine());
+if (num >=100 | num<1000)
+{
+    int result = (num%100 - num%10)/10;
+    System.Console.WriteLine(result);
+}
+*/
+
+//вывод третьей цифры числа или сообщение, то третьей цифры нет
+string num = Console.ReadLine();
+int length = num.Length;
+
+if (length>2)
+{
+    System.Console.WriteLine($"RESULT:{num[2]}");
+}
+else
+{
+    System.Console.WriteLine("RESULT: Третьей цифры нет");
+}
