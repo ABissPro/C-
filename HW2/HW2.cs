7cee21f52a1df1cb1fed00143dd6f61372a3cb92
ABiss <solyaris-2000@mail.ru>
2022-08-23 22:46:38 +0300

    final

--- HW2/HW2.cs
+++ HW2/HW2.cs
@@ -6,7 +6,7 @@ if (num >=100 | num<1000)
     int result = (num%100 - num%10)/10;
     System.Console.WriteLine(result);
 }
-*/
+
 
 //вывод третьей цифры числа или сообщение, то третьей цифры нет
 string num = Console.ReadLine();
@@ -20,3 +20,18 @@ else
 {
     System.Console.WriteLine("RESULT: Третьей цифры нет");
 }
+*/
+
+
+//выходной-будний
+int [] array ={1,2,3,4,5,6,7};
+int num = Convert.ToInt32(Console.ReadLine());
+
+  if (num>=array[5] & num<=array[6])
+ {
+    System.Console.WriteLine(" - выходной день");
+ }  
+  if (num>=array[0] & num<=array[4])
+ {
+    System.Console.WriteLine(" - будний день");
+ }
